d67413e8e788292f39207d8aade5ab117772dad7
Timury <Mrkotex.yt@gmail.com>
2023-06-25 17:40:17 +0200

    creation of the plugin

--- src/main/java/dev/timury/customkit/CustomKit.java
+++ src/main/java/dev/timury/customkit/CustomKit.java
@@ -1,13 +1,18 @@
 package dev.timury.customkit;
 
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CustomKit extends JavaPlugin {
+
+    @Getter
+    private static CustomKit instance;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         getLogger().info("Plugin has been enabled");
+        instance = this;
 
     }
 
@@ -15,5 +20,6 @@ public final class CustomKit extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
         getLogger().info("Plugin has been disabled");
+        instance = null;
     }
 }
